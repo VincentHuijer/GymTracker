2696f7e069eaf3a836e996f6be011dc1823a1762
Helloguis <94686622+VincentHuijer@users.noreply.github.com>
2024-02-14 21:20:23 +0100

    WIP until mytextinput not showing styled stuff?

--- screens/Login.js
+++ screens/Login.js
@@ -4,11 +4,20 @@ import {
   PageLogo, 
   PageTitle,
   SubTitle,
-  StyledFormArea
+  StyledFormArea,
+  LeftIcon,
+  StyledInputLabel,
+  StyledTextInput,
+  RightIcon,
+  Colors
 } from './../components/styles';
 import React from "react";
 import { StatusBar } from "expo-status-bar";
+import { Formik } from 'formik';
 import { View } from "react-native";
+import {Octicons} from '@expo/vector-icons';
+
+const {brand, darkLight} = Colors;
 
 const Login = () => {
   return (
@@ -19,7 +28,27 @@ const Login = () => {
         <PageTitle> Flower Crib </PageTitle>
         <SubTitle> Account Login </SubTitle>
         
-       
+        <Formik
+          initialValues = {{ email: '', password: '' }}
+          onSubmit = {(values) => { 
+            console.log(values);
+          }}
+        >
+          {({handleChange, handleBlur, handleSubmit, values}) => {
+            <StyledFormArea>
+              <MyTextInput
+                label="Email Address"
+                icon="mail"
+                placeholder="ArnoldS@gmail.com"
+                placeholderTextColor={darkLight}
+                onChangeText={handleChange('email')}
+                onBlur={handleBlur('email')}
+                value={values.email}
+                keyboardType="email-address"
+              />
+            </StyledFormArea>
+          }}
+        </Formik>
       </InnerContainer>
     </StyledContainer>
   );
@@ -28,7 +57,12 @@ const Login = () => {
 const MyTextInput = ({label, icon, ...props}) => {
   return (
     <view>
+      <LeftIcon>
+        <Octicons name={icon} size={30} color={brand}/>
+      </LeftIcon>
 
+      <StyledInputLabel>{label}</StyledInputLabel>
+      <StyledTextInput {...props} />
     </view>
   )
 }
