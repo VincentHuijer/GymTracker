25b1bc5f347909a1d89f7c10d26ec2f833045d6d
Helloguis <94686622+VincentHuijer@users.noreply.github.com>
2024-03-11 14:27:45 +0100

    Update MyWorkouts.js

--- screens/workoutScreens/MyWorkouts.js
+++ screens/workoutScreens/MyWorkouts.js
@@ -18,38 +18,6 @@ const MyWorkoutsScreen = () => {
       <WorkoutComponent workoutName='MySpicyWorkout' workoutLink='NonGenericWorkout' workoutIcon={<LegPressIcon/>}/>
       <WorkoutComponent workoutName='MySpicyWorkout' workoutLink='NonGenericWorkout' workoutIcon={<MuscleIcon/>}/>
       <WorkoutComponent workoutName='Add New Workout' workoutLink='NonGenericWorkout' workoutIcon={<PlusIcon/>}/>
-
-      
-      {/* <TouchableOpacity style={styles.box} onPress={() => navigation.navigate('NonGenericWorkout')}>
-        <View style={{ flexDirection: 'row', marginLeft: 20,}}>
-          <LegPressIcon style={{ width: 40, height: 40, marginBottom: 2}} />
-          <Text style={[styles.text, {alignSelf: 'center', marginLeft: 10}]}>MyLegWorkout#1</Text>
-        </View>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.box}>
-        <View style={{ flexDirection: 'row', marginLeft: 20,}}>
-          <MuscleIcon style={{ width: 40, height: 40, marginBottom: 2}} />
-          <Text style={[styles.text, {alignSelf: 'center', marginLeft: 10}]}>MyArmWorkout#1</Text>
-        </View>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.box}>
-        <View style={{ flexDirection: 'row', marginLeft: 20,}}>
-          <PullIcon style={{ width: 40, height: 40, marginBottom: 2}} />
-          <Text style={[styles.text, {alignSelf: 'center', marginLeft: 10}]}>MyPullWorkout#1</Text>
-        </View>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.box}>
-        <View style={{ flexDirection: 'row', marginLeft: 20,}}>
-          <PushIcon style={{ width: 40, height: 40, marginBottom: 2}} />
-          <Text style={[styles.text, {alignSelf: 'center', marginLeft: 10}]}>MyPushWorkout#1</Text>
-        </View>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.box}>
-        <View style={{ flexDirection: 'row', marginLeft: 20,}}>
-          <PlusIcon style={{ width: 40, height: 40, marginBottom: 2}} />
-          <Text style={[styles.text, {alignSelf: 'center', marginLeft: 10}]}>Add new workout </Text>
-        </View>
-      </TouchableOpacity> */}
     </View>
   );
 };
