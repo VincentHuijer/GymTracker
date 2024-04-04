e3d9d8eec9212cdf63d83f1fd0f820c8c62045e7
VincentHuijer <20161700@student.hhs.nl>
2024-04-04 15:06:58 +0200

    bodyweighttracker fixed

--- screens/workoutScreens/BodyWeightScreen.js
+++ screens/workoutScreens/BodyWeightScreen.js
@@ -3,6 +3,7 @@ import { Text, View, TextInput, Button } from 'react-native';
 import { supabase } from '../../lib/supabase';
 import moment from 'moment';
 import { SessionContext } from '../../SessionContext';
+import { useContext } from 'react';
 
 const BodyWeightScreen = () => {
   const { session } = useContext(SessionContext);
@@ -12,10 +13,20 @@ const BodyWeightScreen = () => {
 
   const handleSaveWeight = async () => {
     try {
+      if (!session || !session.user) {
+        console.error('No user session found.');
+        return;
+      }
+      
       const formattedDateTime = moment(dateTime).format('DD/MM/YYYY HH:mm:ss');
       const { data, error } = await supabase
-        .from('BodyWeightTracker') // Change table name here
-        .insert([{ weight: parseFloat(weight), notes, recorded_at: formattedDateTime }]);
+        .from('BodyWeightTracker')
+        .insert([{ 
+          weight: parseFloat(weight), 
+          notes, 
+          recorded_at: formattedDateTime,
+          userid: session.user.id
+        }]);
 
       if (error) {
         console.error('Error saving weight:', error.message);
