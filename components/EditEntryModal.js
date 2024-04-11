a19a1a009bd9b94ef6643ab8145a6b506b1eac6c
Helloguis <94686622+VincentHuijer@users.noreply.github.com>
2024-04-11 23:57:41 +0200

    Indentation

--- components/EditEntryModal.js
+++ components/EditEntryModal.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Modal, View, TextInput, Button, Text } from 'react-native';
+import WhiteTextButtonNew from './WhiteTextButtonNew';
 
 const EditEntryModal = ({ visible, onClose, entry, onEdit, onRemove }) => {
   const [editedWeight, setEditedWeight] = useState(entry.weight.toString());
@@ -21,22 +22,33 @@ const EditEntryModal = ({ visible, onClose, entry, onEdit, onRemove }) => {
 
   return (
     <Modal visible={visible} onRequestClose={onClose} animationType="slide">
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: "#1C1C1E" }}>
         <Text>{`Recorded At: ${entry.recorded_at}`}</Text>
         <TextInput
           value={editedWeight}
           onChangeText={setEditedWeight}
           keyboardType="numeric"
           placeholder="Enter weight"
+          style={{ padding: 10, fontSize: 30, backgroundColor: 'white', width: '85%', marginTop: 10 }}
         />
         <TextInput
           value={editedNotes}
           onChangeText={setEditedNotes}
           placeholder="Enter notes"
+          style={{ padding: 10, fontSize: 30, backgroundColor: 'white', width: '85%', marginTop: 10 }}
         />
-        <Button title="Edit" onPress={handleEdit} />
-        <Button title="Remove" onPress={handleRemove} />
-        <Button title="Cancel" onPress={onClose} />
+        <TextInput 
+          value={dateTime} 
+          onChangeText={setDateTime} 
+          placeholder="Enter date & time" 
+          style={{padding: 10, fontSize: 30, 
+          backgroundColor: 'grey', 
+          marginTop: 10}} 
+          editable={false}
+        />
+        <WhiteTextButtonNew text={'Edit'} onPress={handleEdit} style={{ backgroundColor: '#5BE432', width: '85%', marginTop: 10 }} />
+        <WhiteTextButtonNew text={'Remove'} onPress={handleRemove} style={{ backgroundColor: '#E43D32', width: '85%', marginTop: 10 }} />
+        <WhiteTextButtonNew text={'Cancel'} onPress={onClose} style={{ backgroundColor: '#CCCCCC', width: '85%', marginTop: 10 }} />
         <Text> I AM AN EDIT MODAL, I AM AN EDIT MODAL III AAAAM AN EDIT MODAL</Text>
       </View>
     </Modal>
--- components/NewEntryModal.js
+++ components/NewEntryModal.js
@@ -6,11 +6,36 @@ const NewEntryModal = ({ visible, onClose, onSave, weight, setWeight, notes, set
   return (
     <Modal visible={visible} onRequestClose={onClose} animationType="slide">
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: "#1C1C1E" }}>
-        <TextInput value={weight} onChangeText={setWeight} keyboardType="number-pad" placeholder="Enter weight" style={{padding: 10, fontSize: 30, backgroundColor: 'white'}} />
-        <TextInput value={notes} onChangeText={setNotes} placeholder="Enter notes" style={{padding: 10, fontSize: 30, backgroundColor: 'white', marginTop: 10}} />
-        <TextInput value={dateTime} onChangeText={setDateTime} placeholder="Enter date & time" style={{padding: 10, fontSize: 30, backgroundColor: 'white', marginTop: 10}} />
-        <WhiteTextButtonNew text={'save weight'} onPress={onSave} style={{backgroundColor: '#5BE432', width: '85%', marginTop: 10}}/>
-        <WhiteTextButtonNew text={'Cancel'} onPress={onClose} style={{backgroundColor: '#E43D32', width: '85%', marginTop: 10}}/>
+        <TextInput 
+          value={weight} 
+          onChangeText={setWeight} 
+          keyboardType="number-pad" 
+          placeholder="Enter weight" 
+          style={{ padding: 10, fontSize: 30, backgroundColor: 'white' }}
+        />
+        <TextInput 
+          value={notes}
+          onChangeText={setNotes} 
+          placeholder="Enter notes" 
+          style={{ padding: 10, fontSize: 30, backgroundColor: 'white', marginTop: 10 }}
+        />
+        <TextInput 
+          value={dateTime} 
+          onChangeText={setDateTime} 
+          placeholder="Enter date & time" 
+          style={{ padding: 10, fontSize: 30, backgroundColor: 'grey', marginTop: 10 }}
+          editable={false}
+        />
+        <WhiteTextButtonNew 
+          text={'save weight'} 
+          onPress={onSave} 
+          style={{ backgroundColor: '#5BE432', width: '85%', marginTop: 10 }}
+        />
+        <WhiteTextButtonNew 
+          text={'Cancel'} 
+          onPress={onClose} 
+          style={{ backgroundColor: '#E43D32', width: '85%', marginTop: 10 }}
+        />
       </View>
     </Modal>
   );
